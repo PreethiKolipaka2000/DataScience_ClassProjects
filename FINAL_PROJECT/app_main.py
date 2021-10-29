90217a065bf16d28c058a120ac489a409553300c
PreethiKolipaka2000 <86162886+PreethiKolipaka2000@users.noreply.github.com>
2021-10-29 23:09:50 +0530

    Update app_main.py

--- FINAL_PROJECT/app_main.py
+++ FINAL_PROJECT/app_main.py
@@ -1,4 +1,14 @@
  
+ 
+ 
+# PROGRAM        : TO DEVELOP A DASHBOARD AND DISPLAY ALL NECESSARY DETAILS OF ZOMATO BANGLORE RESTAURANT AND PERRFORM DATA ANALYSIS  
+# PROGRAMMED BY  : KOLIPAKA PREETHI
+# EMAIL ID       : b18cs148@kitsw.ac.in 
+# DATE           : 29-10-2021
+# PYTHON VERSION : 3.8
+# CAVEATS        : None 
+# LICENSE        : None 
+
 from flask import Flask , render_template 
 
 app=Flask(__name__)
